5f659a4d00a832b12a21c26871a765748241c729
harshrajdhote <dhoteharshraj@gmail.com>
2021-02-11 21:40:37 +0530

    Stateless widget problem 3.28

--- basic_app/lib/main.dart
+++ basic_app/lib/main.dart
@@ -7,8 +7,45 @@ void main() {
 
 class MyApp extends StatelessWidget {
   @override
+  var questionIndex = 0;
   Widget build(BuildContext context) {
     //home is named arg
-    return MaterialApp(home: Text("Hello!"));
+    var questions = [
+      "what's your favourite color?",
+      "what's your favourite animal?"
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Quiz App"),
+        ),
+        body: Column(
+          children: [
+            Text(questions[questionIndex]),
+            RaisedButton(
+              child: Text("Ans 1"),
+              onPressed: () {
+                questionIndex = questionIndex + 1;
+                print("Answer choosen");
+              },
+            ),
+            RaisedButton(
+              child: Text("Ans 2"),
+              onPressed: () {
+                questionIndex = questionIndex + 1;
+                print("Answer choosen");
+              },
+            ),
+            RaisedButton(
+              child: Text("Ans 3"),
+              onPressed: () {
+                questionIndex = questionIndex + 1;
+                print("Answer choosen");
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
